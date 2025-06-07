19098150706744d690b33fcf031c2dbc57b47966
Ayushman-1744 <ayushman1744@gmail.com>
2025-06-07 12:21:44 +0530

    files added and changes made

--- main.tf
+++ main.tf
@@ -1,7 +1,25 @@
-resource "aws_iam_user" "cloud" {
-    name = "cloud-institute-user"
-    #name = "ayushman"
-    tags = {
-        Name = "cloud-user"
+terraform {
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+      }
     }
+}
+
+provider "aws" {
+    region = "ap-south-1" #change as needed  
+}
+
+terraform {
+  backend "s3" {
+    bucket = "jenkins-terraform-fielstate"
+    key = "tfstate"
+    region = "ap-south-1"   
+  }
+}
+# Create IAM user
+
+resource "aws_iam_user" "terraform_user" {
+    name = "terraform"
 }
